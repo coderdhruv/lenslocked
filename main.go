3e5f0358ebf6e725166feb9fb97a47c428aedc6e
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2020-08-20 00:59:05 +0530

    added a contact page

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 //where our requests will go and write
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1> Welcome to my good site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1> Welcome to my good site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch,please send an email to <a href = \"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {

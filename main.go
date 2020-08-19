813b45f27a6fc8f90b43f713e346af1bf3f8366b
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2020-08-20 01:12:38 +0530

    added 404 page

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1> Welcome to my good site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch,please send an email to <a href = \"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> You are at an invalid page </h1>")
 	}
 }
 
